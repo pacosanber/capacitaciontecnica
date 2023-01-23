345fb8d0026fefe4dd972a53d523cadbcc081d80
pacosanber <odoo@ampd02.odoo.com>
2023-01-23 19:18:10 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf -8 -*-
+
+{
+    'name' : 'Odoo Academy',
+    'sumary' : """Academy app yp manage training""",
+    'description': """
+        Academy module to manage training:
+        -courses
+        -sessions
+        -attendees
+    """,
+    'autor' : 'Francisco Sanchez',
+    'website' : 'https://www.odoo.com',
+    'category' : 'training',
+    'version' : '0.1',
+    'depends' ['base'],
+    'data' : [],
+    'demo' : [],
+}
